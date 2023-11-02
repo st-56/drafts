2cf50da1c8329ee093ba299c4d605cb68d78f546
st-56 <130740734+st-56@users.noreply.github.com>
2023-11-02 23:25:49 +0800

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,14 +1,14 @@
-// 获取元素
+// 獲取元素
 const openexplButton = document.getElementById("openexpl");
 const closeexplButton = document.getElementById("closeexpl");
 const expl = document.getElementById("expl");
 
-// 打开弹出窗口
+// 打開彈出窗口
 openexplButton.addEventListener("click", function() {
   expl.style.display = "block";
 });
 
-// 关闭弹出窗口
+// 關閉彈出窗口
 closeexplButton.addEventListener("click", function() {
   expl.style.display = "none";
 });
