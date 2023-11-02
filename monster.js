bc4446bec650621005e394a2f748c067d541bb5b
st-56 <130740734+st-56@users.noreply.github.com>
2023-11-02 23:14:23 +0800

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,14 +1,14 @@
 // 获取元素
-const openexpl1Button = document.getElementById("openexpl1");
-const closeexpl1Button = document.getElementById("closeexpl1");
-const expl1 = document.getElementById("expl1");
+const openexplButton = document.getElementById("openexpl");
+const closeexplButton = document.getElementById("closeexpl");
+const expl = document.getElementById("expl");
 
 // 打开弹出窗口
-openexpl1Button.addEventListener("click", function() {
-  expl1.style.display = "block";
+openexplButton.addEventListener("click", function() {
+  expl.style.display = "block";
 });
 
 // 关闭弹出窗口
-closeexpl1Button.addEventListener("click", function() {
-  expl1.style.display = "none";
+closeexplButton.addEventListener("click", function() {
+  expl.style.display = "none";
 });
